05cfbd1838aedf61989f36dc6bafa5814ad22628
Samela Farias <124313487+Samelafarias@users.noreply.github.com>
2026-04-22 11:23:11 -0300

    Add test for mixed case letters in CompareSubstrings
    Added a test case to validate behavior with mixed case letters.

--- Maven-JUnit/src/test/java/Exemplo/CompareSubstringsTest.java
+++ Maven-JUnit/src/test/java/Exemplo/CompareSubstringsTest.java
@@ -9,14 +9,18 @@ class CompareSubstringsTest {
     @Test
     void testeExemploPdf() {
         String resultado = cs.getSmallestAndLargest("welcometojava", 3);
-        // O PDF indica que a saída deve ser ava e wel 
         assertEquals("ava\nwel", resultado);
     }
 
     @Test
     void testeStringCurta() {
-        // Se a string tem tamanho exato k, a maior e menor são iguais
         String resultado = cs.getSmallestAndLargest("hello", 5);
         assertEquals("hello\nhello", resultado);
+    }
+    
+    @Test
+    void testeLetrasMaiusculasEMinusculas(){
+    	String resultado = cs.getSmallestAndLargest("ABCDEfghijk", 2);
+    	assertNotNull(resultado);
     }
 }
